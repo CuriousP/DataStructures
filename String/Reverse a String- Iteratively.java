17ce1f260c61cf4b390141cc5d982acb6c735c95
Pooja Diwakar <poojadiwakar@gmail.com>
2017-01-27 20:08:41 -0800

    Using StringBuilder
    Also added program using String Builder

--- String/Reverse a String- Iteratively.java
+++ String/Reverse a String- Iteratively.java
@@ -13,3 +13,21 @@ public String reverseString( String s)
   }
   return result;
 }
+
+
+// Reversing using string BUilder
+
+public String reverseString( String s)
+{
+  if(s == null)
+  {
+    return null;
+  }
+  StringBuilder sb = new StringBuilder();
+
+  for(int i = s.length() -1; i >= 0; i--)
+  {
+    sb = sb.append(s.charAt(i));
+  }
+  return sb.toString() ;
+}
